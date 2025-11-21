9d92210870c54001d90528480275a85682863bda
Rodrigueskwz <kawarodrigues1803@gmail.com>
2025-11-21 16:37:26 -0300

    alter commit

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use(cors());
 // Middleware para ler JSON do corpo da requisição
 app.use(express.json());
 
-const_dirname = path.resolve();
+const _dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(_dirname, "public")));
 
 // GET - Consulta os dados cadastrados
 app.get("/agendamentos", async (req, res) => {
